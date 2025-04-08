5cf0e82ba2115f2694d8e4894e1d089faa9e8712
Alex Abatov <a.aliksant315@gmail.com>
2025-04-09 07:48:15 +0900

    Added ImgagePattern component for SignUppage

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js"
+import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
@@ -10,22 +10,20 @@ const app = express();
 
 dotenv.config();
 
-   
 app.use(express.json());
 app.use(cookieParser());
 const PORT = process.env.PORT;
 app.use(
-	cors({
-		origin: "http://localhost:5173",
-		credentials: true,
-	})
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
 );
-   
+
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-app.listen(PORT, () => 
-{ 
-    connectDB();
-    console.log("server is running in PORT port" +PORT);
-})
+app.listen(PORT, () => {
+  connectDB();
+  console.log("server is running in PORT port" + PORT);
+});
--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
 import { Link } from "react-router-dom";
+import AuthImagePattern from "../components/AuthImagePattern";
 
 import {
   Eye,
