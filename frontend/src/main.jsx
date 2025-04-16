519c5320d8960d5dabde10d95d59193906d70be0
Alex Abatov <a.aliksant315@gmail.com>
2025-04-16 23:44:59 +0900

    Added loginPage ui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,12 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import SignupPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <SignupPage />; <App />
+      <LoginPage />; <App />
       {/*if we take away SingnupPage the login will not show  */}
       {/* But in order it successfuly created a user we need to take away SignupPage */}
     </BrowserRouter>
