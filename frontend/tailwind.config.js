912758689ac46a18fe766453dac45083ae3cc0b0
loki0105 <63663677+loki0105@users.noreply.github.com>
2025-04-29 21:46:25 +0900

    just  started with  themes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,9 +2,12 @@ import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [daisyui],
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [daisyui],
+	daisyui: {
+		theme: ["light", "dark", "cupcake", "retro"],
+	},
 };
