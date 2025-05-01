3b458d19a7b063785b33244b3ca40714e6a78f8c
Alex Abatov <a.aliksant315@gmail.com>
2025-05-01 17:32:48 +0900

    Created ui for settingPage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,15 +7,15 @@ import SignupPage from "./pages/SignUpPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import Navbar from "./components/Navbar.jsx";
+import SettingsPage from "./pages/SettingsPage.jsx";
 
 createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<BrowserRouter>
-
-			<App />
-			{/*if we take away SingnupPage the login will not show  */}
-			{/* But in order it successfuly created a user we need to take away SignupPage */}
-		</BrowserRouter>
-	</StrictMode>
+  <StrictMode>
+    <BrowserRouter>
+      {/* <App /> */}
+      {/*if we take away SingnupPage the login will not show  */}
+      {/* But in order it successfuly created a user we need to take away SignupPage */}
+      <SettingsPage />
+    </BrowserRouter>
+  </StrictMode>
 );
-
