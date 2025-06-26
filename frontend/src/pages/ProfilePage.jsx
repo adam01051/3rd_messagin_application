3faa7ec4938ede53c5d28dd47702747dfec30063
loki0105 <63663677+loki0105@users.noreply.github.com>
2025-06-27 00:30:08 +0900

    starting again

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
 import { Camera, Mail, User } from "lucide-react";
 
+// starting again
+
 
 const ProfilePage = () => {
 	const { authUser, isUpdatingProfile, updateProfile } = useAuthStore();
