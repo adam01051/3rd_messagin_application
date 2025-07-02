e7a8b92ee3192a697d79e9cc454143d502ab07d9
loki0105 <63663677+loki0105@users.noreply.github.com>
2025-07-03 02:52:04 +0900

    done working

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -19,25 +19,24 @@ export const getUsersForSidebar = async (req, res) =>
 }
 
 export const getMessages = async (req, res) => {
-    try {
-        const { id: userToChatId } = req.params;
-        const myId = req.user._id;
-        
+	try {
+		const { id: userToChatId } = req.params;
+		const myId = req.user._id;
 
-        const messages = await Message.find({
-					$or: [
-						{ senderId: myId, receiverId: userToChatId },
-						{ senderId: userToChatId, receiverId: myId },
-					],
-				});
-
-
-		res.status(200).json(filteredUsers);
+		const messages = await Message.find({
+			$or: [
+				{ senderId: myId, receiverId: userToChatId },
+				{ senderId: userToChatId, receiverId: myId },
+			],
+		});
+		res.status(200).json(messages);
 	} catch (error) {
 		console.log("error message controller  ");
 		res.status(500).json({ error: "internal server error" });
 	}
 };
+
+
 
 
 export const sendMessages = async (req, res) => {
