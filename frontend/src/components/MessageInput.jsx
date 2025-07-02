2f33d30c00e60292857594c28946e304e90f52bb
loki0105 <63663677+loki0105@users.noreply.github.com>
2025-07-03 00:04:58 +0900

    doing  messaging  container

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
 
 const MessageInput = () => {
-  return (
-    <div>MessageInput</div>
-  )
-}
+	const [text, setText] = useState("");
+	const [imagePreview, setImagePreview] = useState(null);
+	const [fileInputRef] = useRef(null);
+	const { sendMessages } = useChatStore();
+	const handleImageChange = (e) => {};
+	const removeImage = () => {};
+	const handleSendMessage = async () => {};
 
-export default MessageInput
+	return (
+		<div className="p-4 w-full">
+			{imagePreview && (
+				<div className="mb-3 flex items-center gap-2">
+					<div className="relative">
+						<img
+							src={imagePreview}
+							alt="Preview"
+							className="w-20 h-20 object-cover rounded-lg border border-zinc-700"
+						/>
+						<button
+							onClick={removeImage}
+							className="absolute -top-1.5 -right-1.5 w-5 h-5 rounded-full bg-base-300 flex items-center justify-center"
+							type="button"
+						>
+							<X className="size-3" />
+						</button>
+					</div>
+				</div>
+			)}
+			<form onSubmit={handleSendMessage} className="flex items-center gap-2">
+				<div className="flex-1 flex gap-2">
+					<input
+						type="text"
+						className=" w-full input input-bordered rounded-lg input-sm sm:input-md"
+						placeholder="Type a message"
+						value={text}
+						onChange={(e) => setText(e.target.value)}
+					/>
+					<input
+                        type="file"
+                        accept="image/*"
+						className=" hidden"
+					
+						ref={fileInputRef}
+						onChange={handleImageChange}
+					/>
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default MessageInput;
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -38,6 +38,19 @@ export const useChatStore = create((set) => ({
       set({ isMessagesLoading: false });
     }
   },
+  sendMessages: async (messageData) => {
+    const { selectedUser, messages } = get();
+    try {
+      const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
+      set({ messages: [...messages, res.data] });
+
+
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+
+    
+  },
 
   //todo: optimize this one later
   setSelectedUser: (selectedUser) => set({ selectedUser }),
