d3508bb4f21100b2efe98cda8ce7e57a19230e85
loki0105 <63663677+loki0105@users.noreply.github.com>
2025-07-12 21:49:56 +0900

    was a mistake somewhere

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import SignupPage from "./pages/SignUpPage.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
-import HomePage from "./pages/HomePage.jsx";
-import Navbar from "./components/Navbar.jsx";
-import SettingsPage from "./pages/SettingsPage.jsx";
+
 
 createRoot(document.getElementById("root")).render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+	<StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</StrictMode>
 );
