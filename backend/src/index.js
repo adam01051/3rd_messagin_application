cdb13eb9c732d64c74b81063e0f985096316e689
loki0105 <63663677+loki0105@users.noreply.github.com>
2025-07-12 21:38:07 +0900

    deploying steps

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,15 +6,25 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app,server} from "./lib/socket.js";
-dotenv.config();
 
+import path from "path";
+
+dotenv.config();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
-
+if (process.env.NODE_ENV === "production")
+{
+	app.use(express.static(path.join(__dirname, "../frontend/dist")));
+	app.get("*", (req, res) =>
+	{
+		res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+	})
+}
 
 app.use(cookieParser());
 app.use(
