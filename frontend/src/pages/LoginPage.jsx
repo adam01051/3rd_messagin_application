0305dc02c82f2f85494fcee6345554b360c8008e
loki0105 <63663677+loki0105@users.noreply.github.com>
2025-07-16 20:16:01 +0900

    login page diff

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -18,7 +18,7 @@ const LoginPage = () => {
   };
 
   return (
-		<div className="h-30 grid lg:grid-cols-2">
+		<div className="min-h-screen grid lg:grid-cols-2">
 			{/* Left Side - Form */}
 			<div className="flex flex-col justify-center items-center p-6 sm:p-12">
 				<div className="w-full max-w-md space-y-8">
@@ -118,7 +118,6 @@ const LoginPage = () => {
 
 			{/* Right Side - Image/Pattern */}
 			<AuthImagePattern
-	
 				title={"Welcome back!"}
 				subtitle={
 					"Sign in to continue your conversations and catch up with your messages."
