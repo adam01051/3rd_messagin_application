892bc74d6d22cdd8c9183da57cc1b819fe94ee65
loki0105 <63663677+loki0105@users.noreply.github.com>
2025-07-16 18:33:49 +0900

    try3

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-
-
 export const axiosInstance = axios.create({
 	baseURL:
 		import.meta.env.MODE === "development"
 			? "http://localhost:5001/api"
-			: "/api",
+			: "https://app1-zaix.onrender.com/api", // ✅ production backend
 	withCredentials: true,
 });
