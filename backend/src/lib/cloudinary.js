2f86ba972fcea91f616e5433aba9580588494c7f
loki0105 <63663677+loki0105@users.noreply.github.com>
2025-07-16 18:59:43 +0900

    ss

--- backend/src/lib/cloudinary.js
+++ backend/src/lib/cloudinary.js
@@ -1,10 +1,13 @@
-import { v2 as Cloudinary } from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 
 import { config } from "dotenv";
+
 config();
-Cloudinary.config({
+
+cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_NAME,
 	api_key: process.env.CLOUDINARY_KEY,
 	api_secret: process.env.CLOUDINARY_SECRET,
 });
-export default Cloudinary;
+
+export default cloudinary;
