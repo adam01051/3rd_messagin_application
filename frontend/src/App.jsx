f5e5be3022ce1739cf2a862cf06f8d4d739d0643
loki0105 <63663677+loki0105@users.noreply.github.com>
2025-07-16 17:39:21 +0900

    zzzz

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
 
 
 
-	if (!authUser && isCheckingAuth)
+	if (!authUser && isCheckingAuth )
 		return (
 			<div className="flex items-center justify-center h-screen">
 				<Loader className="size-10 animate-spin" />
@@ -40,22 +40,20 @@ const App = () => {
 			<Routes>
 				<Route
 					path="/"
-					element={authUser !== true ? <HomePage /> : <Navigate to="/login" />}
+					element={authUser ? <HomePage /> : <Navigate to="/login" />}
 				/>
 				<Route
 					path="/signup"
-					element={!authUser !== true ? <SignUpPage /> : <Navigate to="/" />}
+					element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
 				/>
 				<Route
 					path="/login"
-					element={!authUser !== true ? <LoginPage /> : <Navigate to="/" />}
+					element={!authUser ? <LoginPage /> : <Navigate to="/" />}
 				/>
 				<Route path="/settings" element={<SettingsPage />} />
 				<Route
 					path="/profile"
-					element={
-						authUser !== true ? <ProfilePage /> : <Navigate to="/login" />
-					}
+					element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
 				/>
 			</Routes>
 
